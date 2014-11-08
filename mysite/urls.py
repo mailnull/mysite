b7a17bbb50bdd7386a7ab3af93b8b57dd09c94a1
mailnull <67731164@qq.com>
2014-11-08 14:08:22 +0800

    重做模板，重定views

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,15 +2,17 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 #from mysite.views import hello,current_datetime,cpu_temp
 from mysite import views
-
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    #url(r'^admin/', include(admin.site.urls)),
-    (r'^$',views.hello ),
-    (r'^hello/$',views.hello),
-    ('^time/$',views.current_datetime),
-    (r'^temp/$',views.cpu_temp ),
+    url(r'^admin/', include(admin.site.urls)),
+    # (r'^$',views.hello ),
+    #(r'^hello/$',views.hello),
+    #('^time/$',views.current_datetime),
+    #(r'^temp/$',views.cpu_temp ),
+    (r'^show/(\d{1,2})/$',views.show),
+    (r'^mailnull/(.+)/(.+)/$',views.mailnull),
+    (r'^$',views.index),
 )
--- mysite/views.py
+++ mysite/views.py
@@ -1,18 +1,57 @@
-from django.shortcuts import render_to_response
+# _*_ coding:UTF-8 _*_
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+from django.shortcuts import render_to_response,render
 from django.http import HttpResponse
+from django import template
+from django.template import RequestContext
 import datetime
+
+
 
 def hello(request):
     return HttpResponse("Hello world")
 
-def current_datetime(request):
+#def current_datetime(request):
+ #   now = datetime.datetime.now()
+  #  timeofme="%s" %now
+   # return render_to_response('current_datetime.html', {'current_date': timeofme})
+#def cpu_temp(request):
+ #   file = open("/sys/class/thermal/thermal_zone0/temp","r")
+  #  temp = float(file.read())/1000
+   # file.close()
+    #ftemp = "%.2f" %temp
+    #return   render_to_response('cpu_temp.html', {'cpu_temp': ftemp})
+#系统信息全局
+def sysinfo_proc(request):
     now = datetime.datetime.now()
-    timeofme="%s" %now
-    return render_to_response('current_datetime.html', {'current_date': timeofme})
-def cpu_temp(request):
+    timeofme="%.19s" %now
     file = open("/sys/class/thermal/thermal_zone0/temp","r")
     temp = float(file.read())/1000
     file.close()
     ftemp = "%.2f" %temp
-    return   render_to_response('cpu_temp.html', {'cpu_temp': ftemp})
+    ip= request.META['REMOTE_ADDR']
+    agent=request.META['HTTP_USER_AGENT']
+    
+    return {
+            'sys_date': timeofme,
+            'cpu_temp': ftemp,
+            'ipadd': ip,
+            'useragent': agent
+    }
 
+def show(requset,num):
+    num = int(num)
+    num = str(num)
+    msg = "what your input is:"+num
+    return HttpResponse(msg)
+
+def mailnull(requset,num1,num2):
+    num1 = str(num1)
+    num2 = unichr(num2)
+    msg = "the frist param:"+num1+"the second:"+num2
+    return HttpResponse(msg)
+def index(request):
+    return render_to_response('sysinfo.html',
+            context_instance=RequestContext(request,processors=[sysinfo_proc]))
